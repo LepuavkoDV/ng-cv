66c2482ffb0c9cccefc5921eceff50faa7fbbf4e
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2021-04-25 22:59:42 +0300

    1

--- src/app/core/services/skills.service.ts
+++ src/app/core/services/skills.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { CoreService } from './core.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SkillsService {
+export class SkillsService extends CoreService {
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  getSkills() {}
 }
