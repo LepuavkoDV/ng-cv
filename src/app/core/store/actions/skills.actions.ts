9cf0c30144902a4edffd4035c1c9b3d76237d358
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2021-04-26 00:10:12 +0300

    1

--- src/app/core/store/actions/skills.actions.ts
+++ src/app/core/store/actions/skills.actions.ts
@@ -1,15 +1,21 @@
 import { createAction, props } from '@ngrx/store';
 
-export const loadSkillss = createAction(
-  '[Skills] Load Skillss'
+export enum SkillsActionTypes {
+  loadSkills = '[Skills] Loading Skills',
+  loadSkillsSuccess = '[Skills] Loading Success',
+  loadSkillsFailure = '[Skills] Loading Failure',
+}
+
+export const loadSkills = createAction(
+  SkillsActionTypes.loadSkills,
 );
 
-export const loadSkillssSuccess = createAction(
-  '[Skills] Load Skillss Success',
+export const loadSkillsSuccess = createAction(
+  SkillsActionTypes.loadSkillsSuccess,
   props<{ data: any }>()
 );
 
-export const loadSkillssFailure = createAction(
-  '[Skills] Load Skillss Failure',
+export const loadSkillsFailure = createAction(
+  SkillsActionTypes.loadSkillsFailure,
   props<{ error: any }>()
 );
